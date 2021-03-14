5cccc6b3108b41960c6e010fda110ccee81614a9
tlpackus <tlpackus@gmail.com>
2021-03-13 20:26:23 -0800

    show succesful pass

--- src/triangle.js
+++ src/triangle.js
@@ -1,7 +1,7 @@
 export default function Triangle(side1, side2, side3) {
-  // this.side1 = side1;
-  // this.side2 = side2;
-  // this.side3 = side3;
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
 }
 
 // Triangle.prototype.checkType = function() {
